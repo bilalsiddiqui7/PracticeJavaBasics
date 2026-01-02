c989216c1746c1b0f1d750fb1ec150cc48641223
bilalsiddiqui7 <81403195+bilalsiddiqui7@users.noreply.github.com>
2026-01-02 23:21:03 +0530

    Update InternalWorkingOfHashMap.java

--- src/com/practice/InternalWorkingOfHashMap/InternalWorkingOfHashMap.java
+++ src/com/practice/InternalWorkingOfHashMap/InternalWorkingOfHashMap.java
@@ -1,41 +1,99 @@
-//-Imagine HashMap or a Map as a dictionary.
-//
-//-Hashing -> Transformation of a String of characters to a shorted fixed length value that represents original String. A shorter value helps in
-//indexing and faster searches.
-//
-//-In every object java has a public int hashcode() method that will return a hash value for given object.
-//
-//-Equals and HashCode contract -> If two objects are equal they should have a same hashcode as well.
-//
-//-The Hashcode is used in storing the values in HashMap
-//
-//-HashMap will have buckets. Where each bucket will have a linkedList of Nodes.
-//
-//How put works ->
-//Hashmap comes with a size of 2 raise to power n. Default size of HashMap is 16.
-//put(K k, V v){
-//	hash(k);
-//	index=hash & n-1;
-//}
-//When we call put, firstly it computes the hash of the key and then the index is calculated so that we can find exactly where we need to fit the 
-//value in our HashMap
-//If we have same index then the entry will be added as a next node of the already existing node. This is also called as COLLISION.
-//Also, HashMap allows us to store null as keys so the hash will be zero resulting in index as zero.
-//
-//How get works ->
-//V get(Object key) {
-//	hash(key);
-//	index=hash & n-1;
-//}
-//When we call get it also does the same operation as put, firstly it computes the hash of the key and then the index is calculated,
-//we search by index in the HashMap then we compare the hashcode of the key in the hashmap and then we match the key if the key matches 
-//then the value at that node is returned. In the case of collosion we will match the hashcode if it does not matches we will shift to the 
-//next node of the same index and match and so on till the match is found once hashcode matches then we will match the key and if it matches 
-// we will return the value.
+// Imagine a HashMap / Map as a dictionary, where each word (key) maps to a meaning (value).
 
-//In java-8 when we have too many unequal keys which gives same index, when the number of times in a hash bucket grows beyond certain 
-//threshold (TREEIFY_THRESHOLD=8), content of that bucket switches form using a linked list of entry objects to a balanced tree. This theoritically
-//improves the worst case performance of O(n) to O(log n). Balanced search tree, where left nodes have lesser weight(hashcode) for the keys involved.
+// Hashing is the process of converting an object (not just a String) into a fixed-length integer value called a hash code.
+// This hash value helps in indexing and faster searching.
+
+// In Java, every object has a public int hashCode() method that returns a hash value for that object.
+
+// equals() and hashCode() contract:
+// If two objects are equal using equals(), they must return the same hashCode().
+
+// The hash code is used by HashMap to determine where the key-value pair should be stored.
+
+// A HashMap internally maintains an array of buckets.
+// Each bucket may contain:
+
+// No entry
+
+// A single node
+
+// A linked list of nodes
+
+// A balanced tree (Java 8+)
+
+// 🔹 How put() Works
+
+// HashMap capacity is always a power of 2.
+// The default initial capacity is 16.
+
+// put(K key, V value) {
+//     hash(key);
+//     index = (n - 1) & hash;
+// }
+
+
+// When put() is called:
+
+// HashMap computes the hash value of the key.
+
+// The index is calculated using (n - 1) & hash.
+
+// This index determines which bucket the entry belongs to.
+
+// If another entry already exists at the same index, a collision occurs.
+
+// The new entry is added as the next node in that bucket.
+
+// HashMap allows one null key.
+
+// For a null key, the hash value is treated as 0, so it is stored at index 0.
+
+// 🔹 How get() Works
+// V get(Object key) {
+//     hash(key);
+//     index = (n - 1) & hash;
+// }
+
+
+// When get() is called:
+
+// The hash of the key is computed.
+
+// The index is calculated using the same logic as put().
+
+// HashMap looks at the bucket at that index.
+
+// It compares:
+
+// First the hash value
+
+// Then the key using equals()
+
+// If a match is found, the corresponding value is returned.
+
+// In case of collision, HashMap traverses:
+
+// Each node in the bucket
+
+// Compares hash and then key
+
+// Continues until a match is found
+
+// 🔹 Java 8 Optimization (Treeification)
+
+// In Java 8, if too many keys end up in the same bucket:
+
+// And the bucket size exceeds TREEIFY_THRESHOLD = 8
+
+// The linked list is converted into a balanced Red-Black Tree
+
+// This improves worst-case performance:
+
+// From O(n) (linked list)
+
+// To O(log n) (balanced tree)
+
+// This ensures HashMap remains efficient even with poor hash distributions.
 
 package com.practice.InternalWorkingOfHashMap;
 
